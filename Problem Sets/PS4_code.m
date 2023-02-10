f50ebb8c122e0f272cdf2beb293c6bca054a4886
Will <willgraham345@gmail.com>
2023-02-10 15:36:30 -0700

    Worked on it

---
+++ Problem Sets/PS4_code.m
@@ -0,0 +1,89 @@
+%%% Constants
+clear; clc; format compact; close all;
+k_t = 0.0077; % Motor Torque Constant, N*m/A
+R_a = 2.6; %Armature resistance, Ohms
+N = 70; %Gear ratio
+I_1 = .83e-3; % Inertia, link 1 kg*m^2
+J_m = .65e-6; % Motor Inertia, 2 kg*m^2
+b = 3.1e-6; % mechanical damping, N*m*s/rad
+gravity_torque = 0.067; % gravitational torque constant (m1*g*r01) N*m/rad
+
+%% Problem 1 Work
+N_maximize_torque = sqrt(I_1/J_m);
+
+fprintf('Maximize Torque Gear Ratio: %i\n', round(N_maximize_torque));
+
+
+%% Problem 2.1
+disp("")
+disp("Problem 2a")
+disp("")
+% syms I_1, J_m, N, b, k_t, R_a, gravity_torque
+inertial_terms = (I_1 + J_m*N^2);
+num = [0, 0, (N*k_t)/(R_a*inertial_terms)]; % numerator Open loop
+den = [1, (N^2*(b + k_t^2/R_a)) / inertial_terms, gravity_torque/inertial_terms]; % Denominator open loop
+
+G = tf(num, den); % transfer function
+omega_n = sqrt(den(3));
+zeta = den(2) / (2*omega_n);
+P = pole(G);
+tau = 1 / -P(2);
+G_steady_state = num(3)/ den(3);
+0.63*G_steady_state;
+
+%% Problem 2.2
+disp("")
+disp("Problem 2b")
+disp("")
+num = [0, 0, N*k_t/inertial_terms];
+den = [1, N^2*b/inertial_terms, gravity_torque/inertial_terms];
+H = tf(num, den);
+omega_n = sqrt(den(3));
+zeta = den(2) / (2*omega_n);
+rlocus(H);
+stepinfo(H);
+
+ts = 4 / (zeta*omega_n);
+tp = pi / (omega_n*sqrt(1-zeta^2));
+os = exp(-pi*zeta/(sqrt(1-zeta^2)));
+
+H_steady_state = num(3) / den(3);
+step(H);
+%% Problem 3.1
+disp("")
+disp("Problem 3.1")
+disp("")
+G_p = H
+num = [0, 0, N*k_t/inertial_terms];
+den = [1, N^2*b/inertial_terms, gravity_torque/inertial_terms];
+overshoot_des = .1
+zeta_des = -log(overshoot_des) / sqrt(pi^2 + (log(overshoot_des))^2)
+
+ts_des = 0.2
+omega_n_des = 4/ (ts_des*zeta_des)
+
+den_des = [1, 2*zeta_des*omega_n_des, omega_n_des^2]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
