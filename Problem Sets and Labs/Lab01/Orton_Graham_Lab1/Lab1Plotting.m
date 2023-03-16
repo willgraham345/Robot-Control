bc613aff88eb67d1bd7bd24f64ea9adf086fcd1b
Will <willgraham345@gmail.com>
2023-03-15 23:51:11 -0600

    Lab 1 Finished and turned in

--- Problem Sets and Labs/Lab01/Orton_Graham_Lab1/Lab1Plotting.m
+++ Problem Sets and Labs/Lab01/Orton_Graham_Lab1/Lab1Plotting.m
@@ -2,48 +2,49 @@
 
 figure()
 plot(theta.time,theta.signals.values, 'b')
-xlabel('Time [sec]')
-ylabel('Theta [deg]')
+xlabel('Time (sec)')
+ylabel('Theta (deg)')
 title('Open-Loop Control')
 
 %% Q2
-
+close all; format compact;
 figure()
 hold on
 plot(theta.time,theta.signals.values, 'b')
 plot(theta.time,(rad2deg(.4))*ones(length(theta.time)),'r--')
-xlabel('Time [sec]')
-ylabel('Theta [deg]')
-title('Closed Loop Porportional Control: Gain of P = 2')
+xlabel('Time (sec)')
+ylabel('Theta (deg)')
+title('Closed Loop Porportional Control: Gain of Kp = 0.75')
 legend("Response","Desired Angle")
 hold off
 
 stepinfo(theta.signals.values,theta.time)
 
 %% Q3
-
+close all; format compact;
 figure()
 hold on
 plot(theta.time,theta.signals.values, 'b')
 plot(theta.time,(rad2deg(.2))*ones(length(theta.time)),'r--')
-xlabel('Time [sec]')
-ylabel('Theta [deg]')
-title('PV Control with HW Gains: P-Gain = 8.2, V-Gain = 0.27')
+xlabel('Time (sec)')
+ylabel('Theta (deg)')
+title('PV Control with Tuned Gains: Kp-Gain = 8.5, V-Gain = 0.3')
 legend("Step Response","Desired Angle")
 hold off
 
 stepinfo(theta.signals.values,theta.time)
 
 %% Q4
+close all; format compact
 
 figure()
 hold on
 plot(theta.time,theta.signals.values, 'b')
 plot(theta.time,(rad2deg(.2))*ones(length(theta.time)),'r--')
-xlabel('Time [sec]')
-ylabel('Theta [deg]')
+xlabel('Time (sec)')
+ylabel('Theta (deg)')
 
-title('PIV with HW Gains: P-Gain = 11.75, I-Gain = 97, V-Gain = 0.356')
+title('PIV with Experimental Gains: Kp-Gain = 8.5, V-Gain = 0.3, Ki-Gain = 50')
 legend("Response","Desired Angle")
 hold off
 
